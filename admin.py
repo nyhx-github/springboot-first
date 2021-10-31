de4bb7e7301ce7b0f96e8441e56a23fe89e3622f
v726 <1597297976@qq.com>
2021-10-31 15:41:24 +0800

    添加models文件

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+#导入模型User,Article
+# ort User,Articlefrom blog_api.models imp
+
+admin.site.register(User)
+admin.site.register(Article)
